2916450ccd6e27e0878ad7c895ab9b3f4c67030b
Jacek Nowak <jacek.jan.nowak@gmail.com>
2022-02-09 01:17:01 +0000

    pg search added

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'geocoder'
+gem 'pg_search', '~> 2.3.0'
 group :development, :test do
   gem 'pry-byebug'
   gem 'pry-rails'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
-// import flatpickr from "flatpickr"; //second one is a folder existing in nodule folders, first one is the name of the function inside the folder
+import flatpickr from "flatpickr"; //second one is a folder existing in nodule folders, first one is the name of the function inside the folder
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
