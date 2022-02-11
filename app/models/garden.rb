cd86b285859869f9c24e9077df736c34df024107
Jacek Nowak <jacek.jan.nowak@gmail.com>
2022-02-11 17:38:56 +0000

    extra methods in garden model, for further development

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -14,4 +14,36 @@ class Garden < ApplicationRecord
   #   using: {
   #     tsearch: { prefix: true } 
   #   }
+
+  # i need acces to reviews associated with that garden
+
+  # i need to iterate those reviews with some kind if incrementation
+
+  def number_of_reviews
+    # sum = 0
+    # reviews.each do |review|
+    #   sum += 1
+    # end
+    # return sum 
+    reviews.count
+  end
+
+  def self.gardens_with_review
+    joins(:reviews).where(reviews: {rating: 5})
+    # sum = 0 
+    # Garden.all.each do |garden|
+    #   if garden.reviews.any?
+    #     sum += 1
+    #   end
+    # end
+    # sum
+  end
+
+  def self.five_star_garden
+    joins(:reviews) 
+    SELECT * FROM gardens
+    JOIN reviews ON garden_id = review.garden_id
+    WHERE review.rating = 5 
+  end
 end
+
